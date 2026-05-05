741303b3db7b69d7a285a704addad9792ab6b888
Chralt98 <chralt98@gmail.com>
2026-05-05 16:57:11 +0200

    feat: implement admin post management features with new views and updated routes

--- mvc-pattern-challenge/src/controllers/adminController.ts
+++ mvc-pattern-challenge/src/controllers/adminController.ts
@@ -1,5 +1,27 @@
 import { Request, Response } from "express";
+import { formatDate, slugify, loadPosts } from "../models/postModel.js";
 
-export function showAllPosts(req: Request, res: Response) {
-  res.render("admin.html");
+export function showAdmin(req: Request, res: Response) {
+  const posts = loadPosts();
+  const view = posts.map((post) => ({
+    ...post,
+    slug: slugify(post.title),
+    createdAt: formatDate(post.createdAt),
+  }));
+
+  res.render("admin.html", {
+    posts: view,
+  });
 }
+
+export function showNewPostForm(req: Request, res: Response) {
+  res.render("postForm.html");
+}
+
+export function createNewPost(req: Request, res: Response) {}
+
+export function showEditPostForm(req: Request, res: Response) {}
+
+export function updatePost(req: Request, res: Response) {}
+
+export function deletePost(req: Request, res: Response) {}
--- mvc-pattern-challenge/src/routes/adminRoutes.ts
+++ mvc-pattern-challenge/src/routes/adminRoutes.ts
@@ -3,6 +3,11 @@ import * as adminController from "../controllers/adminController";
 
 const router = express.Router();
 
-router.get("/", adminController.showAllPosts);
+router.get("/", adminController.showAdmin);
+router.get("/posts/new", adminController.showNewPostForm);
+router.post("/posts", adminController.createNewPost);
+router.get("/posts/:slug/edit", adminController.showEditPostForm);
+router.post("/posts/:slug", adminController.updatePost);
+router.post("/posts/:slug/delete", adminController.deletePost);
 
 export default router;
