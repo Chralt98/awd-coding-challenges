80a6d909a35b3e7eab5e8b6977ee529e5f975952
Chralt98 <chralt98@gmail.com>
2026-05-06 16:13:55 +0200

    feat: implement create new post functionality and update form attributes

--- mvc-pattern-challenge/src/controllers/adminController.ts
+++ mvc-pattern-challenge/src/controllers/adminController.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from "express";
-import { loadPosts, addViewMetadata } from "../models/postModel.js";
+import {
+  loadPosts,
+  addViewMetadata,
+  Post,
+  addPost,
+} from "../models/postModel.js";
 
-export async function showAdmin(req: Request, res: Response) {
+export async function showAdmin(_req: Request, res: Response) {
   const posts = await loadPosts();
   const view = addViewMetadata(posts);
 
@@ -10,11 +15,31 @@ export async function showAdmin(req: Request, res: Response) {
   });
 }
 
-export function showNewPostForm(req: Request, res: Response) {
+export function showNewPostForm(_req: Request, res: Response) {
   res.render("postForm.html");
 }
 
-export function createNewPost(req: Request, res: Response) {}
+export async function createNewPost(req: Request, res: Response) {
+  const { title, image, author, teaser, content } = req.body;
+
+  if (!title || !image || !author || !teaser || !content) {
+    res.status(400).send("All fields are required.");
+    return;
+  }
+
+  const newPost = {
+    title,
+    image,
+    author,
+    teaser,
+    content,
+    createdAt: Math.floor(Date.now() / 1000),
+  } as Post;
+
+  await addPost(newPost);
+
+  res.redirect("/admin");
+}
 
 export function showEditPostForm(req: Request, res: Response) {}
 
