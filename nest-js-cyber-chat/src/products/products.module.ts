b866abc199fd041500de6ad64a682acde672f2f4
Christopher Altmann <contact@christopher-altmann.com>
2026-05-22 10:59:07 +0200

    feat(products, users): update ProductsModule to import UsersModule and add exports to UsersModule

--- nest-js-cyber-chat/src/products/products.module.ts
+++ nest-js-cyber-chat/src/products/products.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { ProductsController } from './products.controller';
-import { ProductsService } from './products.service';
+import { Module } from "@nestjs/common";
+import { ProductsController } from "./products.controller";
+import { ProductsService } from "./products.service";
+import { UsersModule } from "../users/users.module";
 
 @Module({
+  imports: [UsersModule],
   controllers: [ProductsController],
-  providers: [ProductsService]
+  providers: [ProductsService],
 })
 export class ProductsModule {}
