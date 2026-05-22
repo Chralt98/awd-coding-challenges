eb28470604212cc2df1dc4d6cb7c37a1a5a975fa
Christopher Altmann <contact@christopher-altmann.com>
2026-05-22 11:00:58 +0200

    feat(products): implement ProductsService with UsersService integration

--- nest-js-cyber-chat/src/products/products.service.ts
+++ nest-js-cyber-chat/src/products/products.service.ts
@@ -1,4 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { UsersService } from "../users/users.service";
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(private readonly usersService: UsersService) {}
+
+  getProductsWithOwners() {
+    const users = this.usersService.getAllUsers();
+    // ... business logic
+  }
+}
