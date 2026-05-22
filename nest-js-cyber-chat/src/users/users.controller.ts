dd8e310751cbbfd358c2eb0cf932d22352ee05ff
Christopher Altmann <contact@christopher-altmann.com>
2026-05-22 11:50:55 +0200

    feat(users): add getById, createUser, and searchUsers endpoints to UsersController

--- nest-js-cyber-chat/src/users/users.controller.ts
+++ nest-js-cyber-chat/src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from "@nestjs/common";
-import { User } from "./user.interface";
+import { Controller, Get, Param, Post, Body, Query } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { User } from "./user.interface";
+import { UserPayload } from "./user-payload.interface";
 
 @Controller("users")
 export class UsersController {
@@ -14,5 +15,21 @@ export class UsersController {
   @Get("random")
   getRandom(): User {
     return this.userService.getRandomUser();
+  }
+
+  @Get(":id")
+  getById(@Param("id") userId: string): User {
+    console.log(userId); // id = "42" for GET /users/42
+  }
+
+  @Post()
+  createUser(@Body() user: UserPayload): User {
+    return this.userService.createUser(user);
+  }
+
+  @Get()
+  searchUsers(@Query("search") search: string): User[] {
+    console.log(search); // search = "alice" for GET /users?search=alice
+    return [];
   }
 }
