0d03cf0dd41ed247d01de4353e4dde9cd399599b
Christopher Altmann <contact@christopher-altmann.com>
2026-05-22 11:15:22 +0200

    feat: implement initial AppModule, AppController, and AppService with basic functionality

--- nest-js-cyber-chat/src/app.module.ts
+++ nest-js-cyber-chat/src/app.module.ts
@@ -1,2 +1,12 @@
+import { Module } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { AppController } from "./app.controller";
+import { UsersModule } from "./users/users.module";
+import { ProductsModule } from "./products/products.module";
 
-import { ProductsModule } from './products/products.module';
+@Module({
+  imports: [UsersModule, ProductsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
--- nest-js-cyber-chat/src/main.ts
+++ nest-js-cyber-chat/src/main.ts
@@ -1,31 +1,5 @@
-import { Module, Injectable, Get, Controller } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
-import { UsersModule } from "./users/users.module";
-import { ProductsModule } from "./products/products.module";
-
-@Injectable()
-class AppService {
-  generateMessage(): string {
-    return "Hello World";
-  }
-}
-
-@Controller()
-class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get("/")
-  showHello() {
-    return this.appService.generateMessage();
-  }
-}
-
-@Module({
-  imports: [UsersModule, ProductsModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
-class AppModule {}
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
