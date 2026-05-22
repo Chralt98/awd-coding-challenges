cfa6d973de578b1334dec6e2be37c5ecbfccec26
Christopher Altmann <contact@christopher-altmann.com>
2026-05-22 11:47:27 +0200

    feat(users): implement UsersController with getAll and getRandom endpoints

--- nest-js-cyber-chat/src/users/users.controller.ts
+++ nest-js-cyber-chat/src/users/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
+import { User } from "./user.interface";
+import { UsersService } from "./users.service";
 
-@Controller('users')
-export class UsersController {}
+@Controller("users")
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get()
+  getAll(): User[] {
+    return this.userService.getAllUsers();
+  }
+
+  @Get("random")
+  getRandom(): User {
+    return this.userService.getRandomUser();
+  }
+}
