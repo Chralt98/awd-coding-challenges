8432a7ee627266612eacbaca97f6d77b0abe51f7
Christopher Altmann <contact@christopher-altmann.com>
2026-06-09 08:53:57 +0200

    fix(tests): re-enable eslint rules for safer type assignments in integration tests

--- nest-js-cyber-chat/src/threads/threads.integration.spec.ts
+++ nest-js-cyber-chat/src/threads/threads.integration.spec.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 import { Test, TestingModule } from '@nestjs/testing';
 import { ThreadsController } from './threads.controller';
 import { ThreadsService } from './threads.service';
