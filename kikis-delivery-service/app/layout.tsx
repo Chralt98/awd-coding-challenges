62746c39f1d1062bb67b415dcd302dfd0004d918
Christopher Altmann <contact@christopher-altmann.com>
2026-06-15 20:12:18 +0200

    feat: update font styling for header and adjust layout component

--- kikis-delivery-service/app/layout.tsx
+++ kikis-delivery-service/app/layout.tsx
@@ -2,8 +2,8 @@ import "./globals.css";
 import { Cherry_Bomb_One } from "next/font/google";
 
 const cherryBomb = Cherry_Bomb_One({
+  variable: "--font-cherry-bomb-one",
   weight: "400",
-  subsets: ["latin"],
 });
 
 export default function RootLayout({
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cherryBomb.className}>
-      <body>
+    <html lang="en">
+      <body className={cherryBomb.variable}>
         <header>
           <h1>Kiki's Delivery Service</h1>
         </header>
